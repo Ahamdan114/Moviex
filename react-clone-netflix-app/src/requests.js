94b14c7a604573c0eaa3cf3bde42fa3c767c963f
Ahmed-Iulian Hamdan <Ahamdan114>
2023-08-16 00:06:56 +0300

    Made the netflix-clone-app

---
+++ react-clone-netflix-app/src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "44e6ee5ef73a49d4ef79d3bac9a81462";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+
+
+export default requests;
